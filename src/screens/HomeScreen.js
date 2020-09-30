e1c5d28459e589b1273c4f7df62ee95ee4afa28c
smulionOong <1224police@likelion.org>
2020-10-01 02:51:34 +0900

    navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Text, StyleSheet, View,Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = props => {
+  //console.log(props.navigation);
+
   return (
     <View>
       <Text style={styles.text}>HomeScreen</Text>
       <Button 
-        onPress = {() => console.log('Button pressed')}
+        onPress = {() => props.navigation.navigate('Components')}
         title = "Go to Components Demo" 
       />
-      <TouchableOpacity onPress={() => console.log('List Pressed')}>
+
+      <Button
+        title = "Go to List Demo"
+        onPress={() => props.navigation.navigate('List')}
+      />
+
+      <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
         <Text>Go to List Demo</Text>
         <Text>Go to List Demo</Text>
         <Text>Go to List Demo</Text>
