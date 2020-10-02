c1bbc80638704339757f0a570d615e26041c181b
smulionOong <1224police@likelion.org>
2020-10-02 22:27:48 +0900

    adding color using setfunction

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);//update the colors using the setColors
+  console.log(colors);
+
+
   return (
   <View>
-    <Button title="Add a Color" />
+    <Button title="Add a Color" onPress={ () => {
+      setColors([...colors, randomRgb()]) 
+      //[]는 새로운 배열이라는 의미임.
+      //...colors = take a look at the colors array, take all the elements inside there. amd add them inside that neww array
+    }}/>
     <View 
-      //style={{ height:100, width:100, backgroundColor:'rgb(0,255,0)' }} 
       style={{ height:100, width:100, backgroundColor:randomRgb() }} 
     />
   </View>
