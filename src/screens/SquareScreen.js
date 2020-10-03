f7f0afa4d477ed19656200c4c834de6d04d614a3
smulionOong <1224police@likelion.org>
2020-10-03 21:10:57 +0900

    creating reducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,48 +1,52 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 const COLOR_INCREMENT = 15;
 
+const reducer = (state, action) => {
+  //state === {red: number, green : number, blue : number};
+  //action === {colorToChange: 'red' || 'green' || 'blue', amount : 15 || -15}
+
+  switch (action.colorToChange){
+    case 'red': 
+      return {...state, red: state.red + action.amount };
+      //never going to do: state.red = state.red -15;
+      //we never going to modify state values directly. so same thing inside reducer
+      // ...state == { red : 0, green:0, blue :0}
+      //return {green:0, blue :0, red: state.red + action.amount} -> 레드는 override
+    case 'green':
+      return {...state, green: state.green + action.amount };
+    case 'blue':
+      return {...state, blue: state.blue + action.amount };
+    default :
+      return state;
+  }
+};
+//리듀서를 밖으로 뺀 이유는 한 컴포넌트 안에 state라고 불리는 두개의 다른 변수가 있으면, 에러는 안나지만 혼란스러울 수 잇음
+//action = how to change state object
 const SquareScreen = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
-
-  const setColor = (color, change) => {
-    // color === 'red', 'blue', 'green'
-    // change === +15, -15
-
-    switch(color){
-      case 'red':
-        red + change > 255 || red + change < 0 ? null : setRed(red + change);
-        return ;
-      case 'green':
-        green + change > 255 || green + change < 0 ? null : setGreen(green + change);
-        return ;
-      case 'blue':
-        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
-        return ;
-      default:
-        return;
-    }
-  };
+  const [state, dispatch] = useReducer(reducer, { red : 0, green:0, blue :0});
+  console.log(state)//{ red : 0, green:0, blue :0}
+  //state == red, green, blue
+  //staet == how we access all different state values. 
+  //우리는 원래, 세 개의 다른 스테이트를 가지고 있었지만, 이제는 그 대신 하나의 오브젝트로 결합된거지.
 
   return (
     <View>
       <ColorCounter 
-        onIncrease = {() => setColor('red', COLOR_INCREMENT)}
-        onDecrease = {() => setColor('red', -1 * COLOR_INCREMENT)}
+        onIncrease = {() => }
+        onDecrease = {() => }
         color = "Red" 
       />
       <ColorCounter 
-        onIncrease={()=>setColor('blue', COLOR_INCREMENT)}
-        onDecrease={()=>setColor('blue', -1 * COLOR_INCREMENT)}
+        onIncrease={()=>}
+        onDecrease={()=>}
         color = "Blue"
       />
       <ColorCounter 
-        onIncrease={()=>setColor('green', COLOR_INCREMENT)}
-        onDecrease={()=>setColor('green', -1 * COLOR_INCREMENT)}
+        onIncrease={()=>}
+        onDecrease={()=>}
         color = "Green"
       />
       <View 
