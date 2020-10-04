be3a15ad6c0c7d55c0f140fd851aa88a2bd53429
smulionOong <1224police@likelion.org>
2020-10-05 01:28:42 +0900

    state -> reducer 셀프 답안

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,27 +1,43 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import {Text, View, StyleSheet, Button} from 'react-native';
 
+const NUMBER_INCREMENT = 1;
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case 'increase_counter':
+      return {...state, counter : state.counter + action.payload}
+    case 'decrease_counter':
+      return {counter : state.counter + action.payload}
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {counter : 0});
+  //counter 값 설정 안해줘서 계속 undefined,nan보여짐.
+  const counter = state;
 
   return (
     <View>
       <Button 
         title = "Increase" 
         onPress={() => {
-          //Don't do this!
-          //counter++;
-          setCounter(counter + 1);
-          //console.log(counter);
+          dispatch({type : `increase_counter`, payload:NUMBER_INCREMENT})
+          //console.log(state.counter);
         }} 
       />
       <Button 
         title = "Decrease" 
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({type : `decrease_counter`, payload: -1 * NUMBER_INCREMENT})
+          //console.log(state.counter);
         }} 
+        //왜 $표시, 백틱 안해도 보여지는거지?
       />
-      <Text>Current Count: {counter}</Text>
+
+      <Text>Current Count: {state.counter}</Text>
     </View>
   )
 };
