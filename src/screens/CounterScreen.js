bec91473e5bc512edc1a9f241aeaa527e8e6acfe
smulionOong <1224police@likelion.org>
2020-10-05 01:41:01 +0900

    state->reducer teacher solution

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,43 +1,43 @@
 import React, { useReducer } from 'react';
 import {Text, View, StyleSheet, Button} from 'react-native';
 
-const NUMBER_INCREMENT = 1;
 
 const reducer = (state, action) => {
+  //state === {count : number}
+  //action === {type:'increment' || 'decrement, payload: 1}
   switch(action.type){
-    case 'increase_counter':
-      return {...state, counter : state.counter + action.payload}
-    case 'decrease_counter':
-      return {counter : state.counter + action.payload}
+    case 'increment':
+      return {...state, count : state.count + action.payload}
+      //여기서 ...state는 사실 필수아님, 왜냐면 우리가 single property를 가지고 있으니까.
+    case 'decrement':
+      return {...state, count : state.count - action.payload}
     default:
       return state;
   }
 };
 
 const CounterScreen = () => {
-  const [state, dispatch] = useReducer(reducer, {counter : 0});
-  //counter 값 설정 안해줘서 계속 undefined,nan보여짐.
-  const counter = state;
+  const [state, dispatch] = useReducer(reducer, {count : 0});//initial state value
 
   return (
     <View>
       <Button 
         title = "Increase" 
         onPress={() => {
-          dispatch({type : `increase_counter`, payload:NUMBER_INCREMENT})
+          dispatch({type : 'increment', payload: 1})
           //console.log(state.counter);
         }} 
       />
       <Button 
         title = "Decrease" 
         onPress={() => {
-          dispatch({type : `decrease_counter`, payload: -1 * NUMBER_INCREMENT})
+          dispatch({type : `decrement`, payload: 1})
           //console.log(state.counter);
         }} 
         //왜 $표시, 백틱 안해도 보여지는거지?
       />
 
-      <Text>Current Count: {state.counter}</Text>
+      <Text>Current Count: {state.count}</Text>
     </View>
   )
 };
