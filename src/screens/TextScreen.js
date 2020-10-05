d6c99c29103daf3b65579edb214d22c3b37c905e
smulionOong <1224police@likelion.org>
2020-10-05 17:58:21 +0900

    textinput exercise by myself

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,21 +2,21 @@ import React, {useState}from 'react';
 import {View, Text, StyleSheet, TextInput} from 'react-native';
 
 const TextScreen = () => {
-  const [name, setName] = useState('');
-
+  const [password, setPassword] = useState('');
 
   return(
     <View>
-      <Text>Enter Name:</Text>
+      <Text>Enter Password:</Text>
       <TextInput 
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value={name}
-        //call-back function
-        onChangeText={(newValue)=>setName(newValue)}
+        value={password}
+        onChangeText={(newValue)=>setPassword(newValue)}
+
       />
-      <Text>My name is {name}</Text>
+      <Text>{password}</Text>
+      {password.length>5? null : <Text>Password must be longer than 5 characters</Text>} 
     </View>
   )
 };
